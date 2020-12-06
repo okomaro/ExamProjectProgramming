04cf92917f3d632c3d52be0e09ef33abf852c82d
okomaro <okomaro@gmail.com>
2020-12-06 16:32:02 +0100

    world to team

---
+++ ExamProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Team!");
+        }
+    }
+}
